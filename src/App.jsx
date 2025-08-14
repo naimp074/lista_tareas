6b998a2f986ee137c23306d1caed870830e2d0b3
Naim Paz <naimpaz274@gmail.com>
2025-08-14 20:26:24 -0300

    agregamos estetica del formulario usando reac-boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
+import Footer from './components/Footer'
+import FormularioTarea from './components/FormularioTarea'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>
-        <h1>hola mundo</h1>
-    </div>
+      <>
+        <main className="container my-4">
+          <h1>Lista de tareas</h1>
+          <FormularioTarea/>
+        </main>
+        <Footer/>
+      </>
   )
 }
 
