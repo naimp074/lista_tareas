0a6674980734bf780c2205f1c60e1718043f48d1
Naim Paz <naimpaz274@gmail.com>
2025-08-14 21:25:59 -0300

    agregamos la validacion del formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,25 +1,52 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import ListaTareas from './ListaTarea';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import ListaTareas from "./ListaTarea";
+import { useForm } from "react-hook-form";
+
 const FormularioTarea = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const posteriorValidacion = (data) => {
+    console.log(data.tarea);
+    //limpiar el formulario
+    reset()
+  };
+
   return (
     <section>
-   <Form>
-        <Form.Group
-          className="mb-3 d-flex justify-content-between"
-        >
-          <Form.Control type="text" placeholder="Ingresa una tarea" />
+      <Form onSubmit={handleSubmit(posteriorValidacion)}>
+        <Form.Group className="mb-3 d-flex justify-content-between">
+          <Form.Control
+            type="text"
+            placeholder="Ingresa una tarea"
+            {...register("tarea", {
+              required: "La tarea es un dato obligatorio",
+              minLength: {
+                value: 2,
+                message: "La tarea debe tener al menos 2 caracteres",
+              },
+              maxLength: {
+                value: 50,
+                message: "La tarea debe tener como máximo 50 caracteres",
+              },
+            })}
+          />
           <Button variant="primary" type="submit">
             ➕
           </Button>
         </Form.Group>
-        <Form.Text className="text-danger">
-          Error al cargar una tarea
-        </Form.Text>
+        <Form.Text className="text-danger">{errors.tarea?.message}</Form.Text>
       </Form>
+
       <ListaTareas />
     </section>
   );
 };
 
 export default FormularioTarea;
+
